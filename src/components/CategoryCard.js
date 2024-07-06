84b9c9cd4d32d85c8c3da6bc29c212a6d76b80a6
Balin Warren <balin.warren@gmail.com>
2024-07-06 14:54:14 -0700

    Added category card component

---
+++ src/components/CategoryCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CategoryCard() {
+    return (
+        <div className="font-display flex size-24 items-center justify-center text-center text-base font-bold sm:size-36 sm:text-xl md:size-48 md:text-2xl">
+            <span>Category</span>
+        </div>
+    );
+}
