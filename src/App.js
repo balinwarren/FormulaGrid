1eabf6696799a60ad5cd82a8a3f09dc1987a8e42
Balin Warren <balin.warren@gmail.com>
2024-07-06 13:37:33 -0700

    Added game title component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './styles.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import GameTitle from './components/GameTitle';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex min-h-screen flex-col items-center justify-between pl-12 pr-12">
         <Header></Header>
+
+        <GameTitle></GameTitle>
 
         <Footer></Footer>
     </div>
