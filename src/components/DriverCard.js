4a5f6af633e53a295b4d557270d0139450588efc
Balin Warren <balin.warren@gmail.com>
2024-07-06 14:53:52 -0700

    Added driver card component

---
+++ src/components/DriverCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DriverCard() {
+    return(
+        <div className="bg-grid-light hover:bg-grid-light-hover dark:bg-grid-dark dark:hover:bg-grid-dark-hover font-display flex size-24 items-center justify-center overflow-hidden border-b border-r border-border-light text-center text-base font-bold sm:size-36 sm:text-lg md:size-48 md:text-xl dark:border-border-dark">
+            <span>Driver</span>
+        </div>
+    );
+}
