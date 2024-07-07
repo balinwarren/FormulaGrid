aebb910b47d0000a5daf857dfd3f17ab70d7c458
Balin Warren <balin.warren@gmail.com>
2024-07-06 19:33:59 -0700

    Fixed header on small screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,35 +7,25 @@ import { faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
 export default function Header(){
     return (
         <nav className="top-0 w-screen bg-header-red">
-            <div className="mx-auto flex max-w-screen-2xl flex-wrap items-center justify-between p-4">
+            <div className="mx-auto max-w-screen-2xl flex items-center justify-between p-4">
                 <a 
                     href="https://formulagrid.app"
                     className="flex items-center space-x-3 rtl:space-x-reverse"
                 >
-                    <div className="h-8 w-64">
+                    <div className="h-auto w-48">
                         <img className="object-contain" src="formula_grid_text_white.webp" alt="Formula Grid" />
                     </div>
                 </a>
 
-                <div className="block w-auto">
-                    <ul className="flex h-auto flex-row p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 rtl:space-x-reverse">
-                        <li>
-                            <a
-                                href="https://twitter.com/formulagridapp"
-                                className="block rounded px-3 py-2 text-white hover:cursor-pointer hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-red-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-red-500"
-                            >
-                                <FontAwesomeIcon icon={faTwitter} transform="grow-10" inverse/>
-                            </a>
-                        </li>
-
-                        <li>
-                            <div 
-                                className="block rounded px-3 py-2 text-white hover:cursor-pointer hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-red-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-red-500"
-                            >
-                                <FontAwesomeIcon icon={faCircleQuestion} transform="grow-10" inverse/>
-                            </div>
-                        </li>
-                    </ul>
+                <div className="flex gap-6">
+                    <a
+                        href="https://twitter.com/formulagridapp"
+                    >
+                        <FontAwesomeIcon icon={faTwitter} transform="grow-10" inverse/>
+                    </a>
+                    <div>
+                        <FontAwesomeIcon icon={faCircleQuestion} transform="grow-10" inverse/>
+                    </div>
                 </div>
             </div>
         </nav>
